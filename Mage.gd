279b303612e208f4f05217e28aa3aca556be039f
3D <drarbego@gmail.com>
2021-09-04 02:14:46 -0500

    initial

---
+++ Mage.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+var speed = 300
+var is_walking = false
+var dir = Vector2.ZERO
+
+func _process(delta):
+	var anim_name = "walking" if self.is_walking else "idle"
+	var anim_dir = "down"
+	if self.dir.y < 0:
+		anim_dir = "up"
+	if self.dir.x != 0:
+		anim_dir = "side"
+	$AnimationPlayer.play(anim_name + "_" + anim_dir)
+
+func _physics_process(delta):
+	var x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	var y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	self.is_walking = x or y
+	self.dir = Vector2(x, y).normalized()
+
+	move_and_slide(dir * speed)
