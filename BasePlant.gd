9d9be0df6055d9736f1336ff6dd3c6dc42936a65
Edgar A. García <egarcia@zenput.com>
2021-09-08 01:09:41 -0500

    Plant production cycle

--- BasePlant.gd
+++ BasePlant.gd
@@ -8,11 +8,11 @@ export var growth_wait_time = 5.0
 const GROWTH_STAGES_COUNT = 3
 var stage = 0 # column index for flowers.png
 
+var produced = false
+
 
 func _ready():
-	print(sprite_row, stage)
 	$ProductionTimer.set_wait_time(self.production_wait_time)
-	$ProductionTimer.start()
 	$GrowthTimer.set_wait_time(self.growth_wait_time)
 	$GrowthTimer.start()
 	self.update_graphics()
@@ -28,11 +28,23 @@ func _on_GrowthTimer_timeout():
 		$GrowthTimer.set_wait_time(self.growth_wait_time)
 		$GrowthTimer.start()
 
+	else:
+		print("production timer started")
+		$ProductionTimer.start()
+
 func update_graphics():
 	$Sprite.set_frame_coords(Vector2(self.stage, self.sprite_row))
 
 func _on_ProductionTimer_timeout():
+	print("producing!")
 	self.produce()
 
+func _process(delta):
+	update()
+
+func _draw():
+	if self.produced:
+		draw_circle(Vector2.ZERO, 24, Color(1, 0, 1, 0.5))
+
 func produce():
-	pass
+	self.produced = true
--- Mage.gd
+++ Mage.gd
@@ -5,6 +5,9 @@ var speed = 300
 var is_walking = false
 var dir = Vector2.ZERO
 
+const FirePepper = preload("res://PlantTypes/FirePepper.tscn")
+
+
 func _process(delta):
 	var anim_name = "walking" if self.is_walking else "idle"
 	var anim_dir = "down"
@@ -16,6 +19,15 @@ func _process(delta):
 	$Sprite.flip_h = (anim_dir == "side" and self.dir.x > 0)
 	$AnimationPlayer.play(anim_name + "_" + anim_dir)
 	$Debug.set_text(anim_name + "_" + anim_dir)
+
+func _unhandled_input(event):
+	if event.is_action_released("ui_select"):
+		self.plant()
+
+func plant():
+	var fire_pepper = FirePepper.instance()
+	fire_pepper.global_position = self.global_position
+	get_parent().add_child(fire_pepper)
 
 func _physics_process(delta):
 	var x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
