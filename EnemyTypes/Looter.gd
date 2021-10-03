27c9878d79b5d291f55f5f5014fe53f67603bbd0
3D <drarbego@gmail.com>
2021-10-03 00:21:54 -0500

    Comments for looter states

--- EnemyTypes/Looter.gd
+++ EnemyTypes/Looter.gd
@@ -8,10 +8,14 @@ func physics_process(delta):
 	self.current_state.physics_process(delta, self)
 
 func on_HitBox_body_entered(body):
+	# damage -> hurting
 	pass
 
 func on_VisionArea_body_entered(body):
+	# if player -> attacking
+	# if plant -> eating
 	pass
 
 func on_VisionArea_body_exited(body):
+	# if player -> wandering
 	pass
