49e0febcdb0c8a1fa5badf0f249667d524201aac
Edgar A. García <drarbego@gmail.com>
2021-10-26 00:59:19 -0500

    Create water drop item

--- WaterDispenser.gd
+++ WaterDispenser.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
 
-const WaterDrop = preload("res://ProjectileTypes/WaterDrop.tscn")
+const WaterDropItem = preload("res://ItemTypes/WaterDropItem.tscn")
 const MAX_AMOUNT = 5
 
 
 func _on_WaterDispenser_body_entered(body):
 	if body is Character:
-		body.add_item_to_inventory(str(WaterDrop), MAX_AMOUNT, MAX_AMOUNT)
+		body.add_item_to_inventory(WaterDropItem, MAX_AMOUNT, MAX_AMOUNT)
